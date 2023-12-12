42e3d9363015976d5d2d352bcebf46a4de5e42cf
tom-Violence <roujux@gmail.com>
2023-12-12 15:36:51 +0100

    ""

--- juju111223-1/references.js
+++ juju111223-1/references.js
@@ -2,7 +2,6 @@ let utilisateur = {
     nom: "jux",
     age: 44,
     email: "jux@juxcompany.com"
-
 }
 
 let  copieutilisateur = utilisateur;
@@ -20,11 +19,12 @@ autreUtilisateur.email = "gloubi@boulga.com";
 console.log(utilisateur.email + " ==> " + autreUtilisateur.email);
 
 
+
 /*    Comment les modifications sur copieUtilisateur et autreUtilisateur affectent-elles utilisateur original ?
     Quelle est la différence entre une copie par référence et une copie profonde ?
 
 la copie par référence ne change pas la référence des elements contenus dans l'objet.
-La copie profonde copie tous les elements en changeant la référence au x éléments copies.
+La copie profonde copie tous les elements en changeant la référence aux éléments copiés.
 
 
     */
